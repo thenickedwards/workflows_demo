927454c5ec78b21f7562c025c9f135257d5a9064
Nick Edwards <nickedwards.dev@gmail.com>
2025-01-13 19:24:52 -0800

    Update helloworld.py, get Python version

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello World from Nick Edwards!")
+import sys
 
+def main():
+    print("Hello World! Nick wants you to know this message is powered by Python: " + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        # let's make this script fail for Python 3.6
+        raise Exception('Python version 3.6.x is unsupported!')
 
 if __name__ == "__main__":
     main()
