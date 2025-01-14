50453b98c26715eb1df880767282e5c6d1aad7cb
Nick Edwards <nickedwards.dev@gmail.com>
2025-01-13 19:26:51 -0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,6 @@ def main():
     if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
         # let's make this script fail for Python 3.6
         raise Exception('Python version 3.6.x is unsupported!')
-
+        
 if __name__ == "__main__":
     main()
